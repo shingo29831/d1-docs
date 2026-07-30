3f1af291d2a89b3236f2cee3f77dcc5cbca6c5a5
shingo29831 <shingo29831@gmail.com>
2026-07-30 20:09:06 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    mermaid.initialize({ 
+        startOnLoad: true,
+        theme: 'default',
+        securityLevel: 'loose',
+        fontFamily: 'Noto Sans JP'
+    });
+});
